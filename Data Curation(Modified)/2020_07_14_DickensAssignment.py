c56f744a3a7a93d9b976f97d41a3892fdc052d37
Shawon Ibn Kamal <shawon.1165@gmail.com>
2021-10-15 19:44:54 -0230

    initial commit

---
+++ Data Curation(Modified)/2020_07_14_DickensAssignment.py
@@ -0,0 +1,111 @@
+import os
+import csv
+
+def patch_name(a):
+    if a == "H":
+        return "Head"
+    elif a == "R":
+        return "Rump"
+    elif a == "T":
+        return "Upper Tail"
+    elif a == "U":
+        return "Chest"
+    elif a == "S":
+        return "Sub-terminal tail band"
+    elif a == "M":
+        return "Mantle"
+    elif a == "B":
+        return "Belly"
+    elif a == "MB":
+        return "Male Belly"
+
+# copy the filenames from the directory
+filenames = [name for path, subdirs, files in os.walk("files")
+             for name in files]
+
+print(len(filenames), "no. of files")
+
+# opens the csv of meta data and stores it in list data
+with open('template.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    data = list(reader)
+
+# opens a csv for filenames that do not have a meta data
+csvfile = open('NotMatched.csv', 'w', newline='')
+notmatched = csv.writer(csvfile)
+notmatched.writerow(['notmatched'])
+
+# counts
+matchFound = 0
+matchNotFound = 0
+
+# copies the list of meta data to another list
+# will contain the list of meta data that do not have any files
+missingFiles = list(data)
+
+
+def compareWithTemplate(writer, patch, id, replicate, filename,modify):
+    global matchFound, matchNotFound
+    matchBool = False
+    for i in data:  # loops through the list of meta data
+        templateId = i[1]+i[3]
+        if id == templateId:  # if match found
+            matchBool = True
+            matchFound += 1
+            i[0] = modify
+            i[20] = patch_name(patch)
+            i[25] = replicate
+            writer.writerow(i)  # add infos and write it to Result.csv
+            exit
+
+    if(matchBool == False):  # if match not found
+        matchNotFound += 1
+        notmatched.writerow([filename])  # add filename to NotMatched.csv
+
+    for j in missingFiles:
+        templateId = j[1]+j[3]
+        if id == templateId:
+            missingFiles.remove(j)
+            exit
+
+
+with open('Result.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(data[0])
+
+    # loop through list of filenames
+    for i in range(len(filenames)):
+        splittedFilename = filenames[i].split(".")
+        if (splittedFilename[4] == "spec"):
+            patch = splittedFilename[1]
+            id = splittedFilename[2]
+            modify = splittedFilename[0]+"."+splittedFilename[1]+"."+splittedFilename[2]+"."+splittedFilename[3]+ "."+splittedFilename[4]
+            replicate = splittedFilename[3]
+        elif(splittedFilename[4] == "csv"):
+            patch = splittedFilename[1]
+            id = splittedFilename[2]
+            modify = splittedFilename[0]+"."+splittedFilename[1]+"."+splittedFilename[2]+"."+splittedFilename[3]
+            replicate = splittedFilename[3]
+        elif((len(splittedFilename[1]) == 1) and (len(splittedFilename[2]) == 1)):
+             patch = splittedFilename[1] + splittedFilename[2]
+             id = splittedFilename[3]
+             modify = splittedFilename[0]+"."+splittedFilename[1]+"."+splittedFilename[2]+"."+splittedFilename[3]+"."+splittedFilename[4]
+             replicate = splittedFilename[4]
+            
+        else:
+            patch = splittedFilename[1]
+            id = splittedFilename[2]
+            modify = splittedFilename[0]+"."+splittedFilename[1]+"."+splittedFilename[2]+"."+splittedFilename[3]
+            replicate = splittedFilename[3]
+        
+
+        compareWithTemplate(writer, patch, id, replicate, filenames[i],modify)
+
+with open('MissingFiles.csv', 'w', newline='') as missingFilesCsv:
+    missingFilesWriter = csv.writer(missingFilesCsv)
+    for row in missingFiles:
+        missingFilesWriter.writerow(row)
+
+print(matchFound, "match found")
+print(matchNotFound, "match not found")
+print("Complete")
---
+++ Data Curation(Modified)/README.txt
@@ -0,0 +1,21 @@
+------------------------------------------------------------------------
+                      2020_07_14_DickensAssignment.py
+------------------------------------------------------------------------
+
+PROJECT TITLE: Curation
+
+PURPOSE OF PROJECT: MUCEP (Dr Pierre-Paul Bitton)
+
+STUDENT NAME : Yusuf Alam 
+
+STUDENT NUMBER ; 201759321
+
+USER INSTRUCTIONS HOW TO RUN :
+
+1. Ensure folder hierarchy is maintained.
+2. Run the file   2020_07_14_DickensAssignment.py.
+3. The resulting files from the program will be files named as MissingFiles.csv(METADATA THAT ARE MISSING), NotMatched.csv (JUST FOR TESTING PURPOSES), Result.csv(MAIN CURATED FILE)
+4. Open Result.csv to see results.
+5. template.csv is the original metadata file that you sent me by email.
+
+. 
